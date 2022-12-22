6e8aa5dd4a871029212cd92d546a2ffc56386897
Marta <turkovska05@gmail.com>
2022-12-23 00:35:32 +0200

    fck u tony

---
+++ UnitTest8.1rek/UnitTest8.1rek.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_08.1(rek)/lab_08.1(rek).cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest81rek
+{
+	TEST_CLASS(UnitTest81rek)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int m = Count("This == +", 0);
+			Assert::AreEqual(m, 3);
+		}
+	};
+}
